e015cbfba92cdd6f6f2922ce6fb75a2be171debe
Marvin Froeder <velo.br@gmail.com>
2025-04-13 10:03:13 -0300

    Refactor copyDirectory to use Files.walkFileTree
    Signed-off-by: Marvin Froeder <velo.br@gmail.com>

--- src/test/java/com/marvinformatics/easyjacoco/ExampleProjectMavenBuildIT.java
+++ src/test/java/com/marvinformatics/easyjacoco/ExampleProjectMavenBuildIT.java
@@ -23,9 +23,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Properties;
 import org.apache.maven.MavenExecutionException;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
@@ -151,17 +154,25 @@ public class ExampleProjectMavenBuildIT {
 
   /** Recursively copies a directory. */
   private static void copyDirectory(Path source, Path target) throws IOException {
-    Files.createDirectories(target);
-    Files.walk(source)
-        .forEach(
-            sourcePath -> {
-              try {
-                Path targetPath = target.resolve(source.relativize(sourcePath));
-                Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
-              } catch (IOException e) {
-                throw new RuntimeException("Error copying directory", e);
-              }
-            });
+    Files.walkFileTree(
+        source,
+        new SimpleFileVisitor<Path>() {
+          @Override
+          public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+              throws IOException {
+            Path targetDir = target.resolve(source.relativize(dir));
+            Files.createDirectories(targetDir);
+            return FileVisitResult.CONTINUE;
+          }
+
+          @Override
+          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+              throws IOException {
+            Path targetFile = target.resolve(source.relativize(file));
+            Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+            return FileVisitResult.CONTINUE;
+          }
+        });
   }
 
   static class TestResult {
